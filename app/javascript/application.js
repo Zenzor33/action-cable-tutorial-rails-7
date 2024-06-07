0cc93ae7d6e2bbbdf620e938e8c6bba45e2e10b8
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2024-06-07 13:35:39 +0200

    finished js section

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+// alert("Hello from application.js");
+
+const message_appender = function (content) {
+  document
+    .querySelector("#messages-table")
+    .insertAdjacentHTML("beforeend", content);
+};
+
+document.addEventListener("turbo:load", function () {
+  message_appender("hello from turbo:load");
+});
