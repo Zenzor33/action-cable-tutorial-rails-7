b013654cc33749b4211eb5eadeb3bed073aac10f
Richard Zeidler <49378556+Zenzor33@users.noreply.github.com>
2024-06-07 12:45:06 +0200

    checkpoint

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,15 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Message < ApplicationRecord
-  # Associations
   belongs_to :user
-
-  # Validations
   validates :content, presence: true
+  scope :for_display, -> { order(:created_at).last(50) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 require 'bcrypt'
 
 class User < ApplicationRecord
-  has_secure_password
-
-  # Associations
   has_many :messages
-
-  # Validations
-  validates :username, presence: true, uniqueness: true
+  NAME_REGEX = /\w+/
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+                       format: { with: /\A#{NAME_REGEX}\z/i },
+                       length: { maximum: 15 }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 end
